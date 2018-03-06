63ef70e4d2130012084151dde9c3460621809dab
strawhatasif <aafasih@gmail.com>
2018-03-06 08:30:52 -0600

    more updates (in progress) for FileSystem assignment

--- src/com/fun/scala/commands/Command.scala
+++ src/com/fun/scala/commands/Command.scala
@@ -7,8 +7,27 @@ trait Command {
 }
 
 object Command {
-  def from(name: String) : Command =
-    new UnknownCommand
+  val MKDIR="mkdir"
+  def emptyCommand : Command = new Command {
+    override def apply(state: State): State = state
+  }
+
+  def incompleteCommand(name: String): Command = new Command {
+    override def apply(state: State): State =
+      state.setMessage(name + ": incomplete command!!!")
+  }
+
+  def from(name: String) : Command = {
+      val tokens: Array[String] = name.split(" ")
+
+      if (name.isEmpty || tokens.isEmpty) emptyCommand
+      else if (MKDIR.equals(tokens(0))) {
+        if (tokens.length < 2) incompleteCommand(MKDIR)
+        else new MkDir(tokens(1))
+      }
+      else new UnknownCommand
+  }
+
 
 }
 
--- src/com/fun/scala/files/DirEntry.scala
+++ src/com/fun/scala/files/DirEntry.scala
@@ -1,6 +1,7 @@
 package com.fun.scala.files
 
-abstract class DirEntry(val parentPath : String, val name : String)
-{
+abstract class DirEntry(val parentPath : String, val name : String) {
+  def path: String = parentPath + Directory.SEPARATOR + name
 
+  def asDirectory: Directory
 }
